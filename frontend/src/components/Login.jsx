da7b9f11f0959287d72417a73dc3fffea7114814
LongHo <longhaiho696@gmail.com>
2022-01-03 10:31:36 +0700

    main commit

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/Share.mp4";
+import { client } from "../client";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className=" relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5 font-extrabold text-white text-5xl shadow-3xl">
+            Spectrum
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  className="bg-white flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                >
+                  <FcGoogle className="mr-4 font-semibold" /> Sign In With
+                  Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/container/Pins.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { CreatePin, Feed, NavBar, PinDetail, Search } from "../components";
+
+function Pins({ user }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="px-2 md:px-2">
+      <div className="bg-gray-50">
+        <NavBar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          user={user}
+        />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user && user} />}
+          />
+          <Route
+            path="/create-pin"
+            element={<CreatePin user={user && user} />}
+          />
+          <Route
+            path="/Search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default Pins;
